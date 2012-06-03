a0b11eeb823eb07b45a92e5861cd4caa1758335b
Andreas Baldeau <andreas@baldeau.net>
2012-06-03 12:02:25 +0200

    Initial import of OpenCV stuff and getting things running.

---
+++ OpenCV-2.4.0-samples/includeOpenCV.mk
@@ -0,0 +1,1 @@
+OPENCV_MK_PATH:=../../OpenCV-2.4.0/share/OpenCV/OpenCV.mk
---
+++ OpenCV-2.4.0-samples/tutorial-3-native/obj/local/armeabi-v7a/objs/native_sample/jni_part.o.d
@@ -0,0 +1,142 @@
+obj/local/armeabi-v7a/objs/native_sample/jni_part.o: jni/jni_part.cpp \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/jni.h \
+ ../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/core/core.hpp \
+ ../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/core/types_c.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/assert.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/stdlib.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/string.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/malloc.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/alloca.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/strings.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/types.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/stdint.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/_types.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/machine/_types.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/posix_types.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/stddef.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/compiler.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/asm/posix_types.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/asm/types.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/types.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/machine/kernel.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/memory.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/math.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/limits.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/limits.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/limits.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/machine/internal_types.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/machine/limits.h \
+ /home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/syslimits.h \
+ ../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/core/version.hpp \
+ /home/baldo/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/include/algorithm \
+ /home/baldo/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/include/cmath \
+ /home/baldo/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/include/complex \
+ /home/baldo/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/include/map \
+ /home/baldo/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/include/vector \
+ ../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/core/operations.hpp \
+ /home/baldo/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/include/limits \
+ ../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/core/mat.hpp \
+ ../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/imgproc/imgproc.hpp \
+ ../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/imgproc/types_c.h \
+ ../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/core/core_c.h \
+ ../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/features2d/features2d.hpp \
+ ../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/flann/miniflann.hpp \
+ ../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/flann/defines.h \
+ ../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/flann/config.h
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/jni.h:
+
+../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/core/core.hpp:
+
+../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/core/types_c.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/assert.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/stdlib.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/string.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/malloc.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/alloca.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/strings.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/types.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/stdint.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/_types.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/machine/_types.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/posix_types.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/stddef.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/compiler.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/asm/posix_types.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/asm/types.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/types.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/machine/kernel.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/memory.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/math.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/limits.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/limits.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/limits.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/machine/internal_types.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/machine/limits.h:
+
+/home/baldo/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/syslimits.h:
+
+../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/core/version.hpp:
+
+/home/baldo/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/include/algorithm:
+
+/home/baldo/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/include/cmath:
+
+/home/baldo/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/include/complex:
+
+/home/baldo/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/include/map:
+
+/home/baldo/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/include/vector:
+
+../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/core/operations.hpp:
+
+/home/baldo/android-ndk-r8/sources/cxx-stl/gnu-libstdc++/include/limits:
+
+../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/core/mat.hpp:
+
+../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/imgproc/imgproc.hpp:
+
+../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/imgproc/types_c.h:
+
+../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/core/core_c.h:
+
+../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/features2d/features2d.hpp:
+
+../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/flann/miniflann.hpp:
+
+../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/flann/defines.h:
+
+../../OpenCV-2.4.0/share/OpenCV/../../include/opencv2/flann/config.h:
